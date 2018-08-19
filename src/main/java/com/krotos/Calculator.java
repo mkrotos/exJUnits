6fc8f7ef7179590d5a651cbada931da36b7491c2
Mariusz Krotos <mar.krotos@gmail.com>
2018-08-19 13:24:57 +0200

    exercises 1

---
+++ src/main/java/com/krotos/Calculator.java
@@ -0,0 +1,17 @@
+package com.krotos;
+
+public class Calculator {
+
+    public double add(int a, int b){
+        return a+b;
+    }
+    public double subtract(int a, int b){
+        return a-b;
+    }
+    public double multiply(int a, int b){
+        return a*b;
+    }
+    public double divide(int a, int b){
+        return a/b;
+    }
+}
---
+++ src/test/java/com/krotos/CalculatorTest.java
@@ -0,0 +1,39 @@
+package com.krotos;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator calculator=new Calculator();
+    static double delta=0.00001;
+
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("BeforeAll");
+        System.out.println("Delta: "+delta);
+    }
+
+    @Test
+    void add() {
+        Assert.assertEquals(5,calculator.add(2,3),delta);
+    }
+
+    @Test
+    void subtract() {
+        Assert.assertEquals(3,calculator.subtract(9,6),delta);
+    }
+
+    @Test
+    void multiply() {
+        Assert.assertEquals(9,calculator.multiply(3,3),delta);
+    }
+
+    @Test
+    void divide() {
+        Assert.assertEquals(2,calculator.divide(4,2),delta);
+    }
+}
